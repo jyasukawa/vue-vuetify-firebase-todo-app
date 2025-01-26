d704f2f1d0834245723908b34d88df20da577dca
jyasukawa <jin.yasukawa123@gmail.com>
2025-01-26 22:18:31 +0900

    remove

--- src/App.vue
+++ src/App.vue
@@ -1,56 +1,11 @@
 <script lang="ts" setup>
   import { ref } from 'vue'
 
-const cards = ['Today', 'Yesterday']
-const links = [
-  ['mdi-inbox-arrow-down', 'Inbox'],
-  ['mdi-send', 'Send'],
-  ['mdi-delete', 'Trash'],
-  ['mdi-alert-octagon', 'Spam'],
-]
-
-const drawer = ref(null)
+const cards = ['Todo']
 </script>
 
 <template>
   <v-app id="inspire">
-    <v-system-bar>
-      <v-spacer></v-spacer>
-
-      <v-icon>mdi-square</v-icon>
-
-      <v-icon>mdi-circle</v-icon>
-
-      <v-icon>mdi-triangle</v-icon>
-    </v-system-bar>
-
-    <v-navigation-drawer v-model="drawer">
-      <v-sheet
-        class="pa-4"
-        color="grey-lighten-4"
-      >
-        <v-avatar
-          class="mb-4"
-          color="grey-darken-1"
-          size="64"
-        ></v-avatar>
-
-        <div>john@google.com</div>
-      </v-sheet>
-
-      <v-divider></v-divider>
-
-      <v-list>
-        <v-list-item
-          v-for="[icon, text] in links"
-          :key="icon"
-          :prepend-icon="icon"
-          :title="text"
-          link
-        ></v-list-item>
-      </v-list>
-    </v-navigation-drawer>
-
     <v-main>
       <v-container
         class="py-8 px-6"
